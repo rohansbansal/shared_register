9126619775eee12313e20c6dd53ad46533331213
Rohan Bansal <rsb344@cornell.edu>
2023-04-29 14:39:13 -0700

    Got grpc set up

--- src/client/main.cc
+++ src/client/main.cc
@@ -1,6 +1,22 @@
+#include <hello.grpc.pb.h>
+
+#include <grpc/grpc.h>
+#include <grpcpp/create_channel.h>
+
 #include <iostream>
 
 int main() {
+  hello::HelloRequest req;
+  hello::HelloResponse res;
+  req.set_name("Rohan");
   std::cout << "Hello World!\n";
+  auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+  std::unique_ptr<hello::Hello::Stub> stub = hello::Hello::NewStub(channel);
+  grpc::ClientContext context;
+  grpc::Status status = stub->SayHello(&context, req, &res);
+
+  // Output result
+  std::cout << "I got:" << std::endl;
+  std::cout << "Response: " << res.response() << std::endl;
 }
 
--- src/server/main.cc
+++ src/server/main.cc
@@ -1,6 +1,32 @@
+#include <grpc/grpc.h>
+#include <grpcpp/server_builder.h>
+
+#include <hello.grpc.pb.h>
+
 #include <iostream>
 
+class HelloService final : public hello::Hello::Service {
+ public:
+  virtual ::grpc::Status SayHello(::grpc::ServerContext* context, const ::hello::HelloRequest* request, ::hello::HelloResponse* response)
+  {
+      std::cout << "Server: GetAddress for \"" << request->name() << "\"." << std::endl;
+
+      response->set_response("Hello World");
+      
+      return grpc::Status::OK;
+  }
+};
+
 int main(int argc, char const* argv[]) {
-  std::cout << "Hello World!\n";
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
+
+  HelloService my_service;
+  builder.RegisterService(&my_service);
+
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  server->Wait();
+  
+  return 0;
 }
 
